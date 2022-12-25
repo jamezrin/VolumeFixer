926a7ebbfa7dcd9df4491bb69d124cedbf652a2c
Jaime Martínez Rincón <jaime@jamezrin.name>
2022-12-25 14:02:44 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use std::fmt::Debug;
+use std::thread::sleep;
+use std::time::Duration;
+
+#[allow(unused_imports)]
 use windows::{
     core::*,
     Win32::*,
+    Win32::UI::*,
+    Win32::UI::Shell::PropertiesSystem::*,
     Win32::System::*,
     Win32::System::Com::*,
     Win32::Foundation::*,
@@ -9,7 +16,9 @@ use windows::{
 };
 
 #[implement(IAudioEndpointVolumeCallback)]
-struct MyVolumeControlCallback;
+struct MyVolumeControlCallback {
+    /*device: IMMDevice*/
+}
 
 #[allow(non_snake_case)]
 impl IAudioEndpointVolumeCallback_Impl for MyVolumeControlCallback {
@@ -17,17 +26,15 @@ impl IAudioEndpointVolumeCallback_Impl for MyVolumeControlCallback {
         // let change = pnotify.read();
         let change = unsafe { pnotify.as_ref().unwrap() };
 
-        println!("volume notification {} {}", change.fMasterVolume, change.nChannels);
+        println!("volume notification {:#?} {} {} {} {}", change.guidEventContext, change.fMasterVolume, change.nChannels, change.bMuted.0, change.afChannelVolumes[0]);
 
         return Ok(());
     }
 }
 
-fn main() {
-    println!("Hello, world!");
-
+fn hook_audio_devices() -> Result<()> {
     unsafe {
-        CoInitializeEx(None, COINIT_MULTITHREADED).expect("TODO: panic message");
+        CoInitializeEx(None, COINIT_MULTITHREADED)?;
 
         let device_enumerator: IMMDeviceEnumerator = CoCreateInstance(
             &MMDeviceEnumerator,
@@ -35,10 +42,49 @@ fn main() {
             CLSCTX_INPROC_SERVER
         )?;
 
+        //device_enumerator.RegisterEndpointNotificationCallback()
+
         let device = device_enumerator.GetDefaultAudioEndpoint(eRender, eConsole)?;
 
-        println!("device {}", device.GetId()?.0)
+        // https://learn.microsoft.com/en-us/windows/win32/api/mmdeviceapi/nf-mmdeviceapi-immdevice-activate
+        let audio_endpoint_volume = device.Activate::<IAudioEndpointVolume>(
+            CLSCTX_ALL,
+            None
+        )?;
+
+        // https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/Media/Audio/Endpoints/struct.IAudioEndpointVolumeCallback.html
+        let volume_control_cb = MyVolumeControlCallback {};
+        let interface : IAudioEndpointVolumeCallback = volume_control_cb.into();
+        audio_endpoint_volume.RegisterControlChangeNotify(&interface)?;
+
+        loop {
+            //println!("audio {}", audio_endpoint_volume.GetMasterVolumeLevelScalar()?);
+            sleep(Duration::from_millis(10));
+        }
+
+        // https://rust-cli.github.io/book/in-depth/signals.html
+
+        /*
+        let device_coll = device_enumerator.EnumAudioEndpoints(
+            eAll,
+            DEVICE_STATE_ACTIVE | DEVICE_STATE_UNPLUGGED
+        )?;
+
+        for ndevice in 0..device_coll.GetCount()? {
+            let device = device_coll.Item(ndevice)?;
+            let property_store = device.OpenPropertyStore(STGM_READ)?;
+
+            println!("ndevice {} {:?}: ", ndevice, device)
+
+        }
+        */
     }
 
-    loop {}
+    Ok(())
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    hook_audio_devices().expect("Fail to hook audio devices");
 }
